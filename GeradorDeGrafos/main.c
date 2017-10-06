cd1dd8e18d188c4db580e73bcf87e3097f034106
Jprnp <jprnp18@gmail.com>
2017-10-06 16:36:28 -0300

    Biblioteca p/ interface com o Grphviz CLEAN

--- GeradorDeGrafos/main.c
+++ GeradorDeGrafos/main.c
@@ -25,289 +25,11 @@
 //*****************************************************************************
 
 int num_vertices = 0;
-//int is_windows = 0;
-
-/*struct col {
-    int id;
-    char cor[15];
-    UT_hash_handle hh;
-};
-
-struct col *colors = NULL;
-
-struct adj_list {
-    int cod;
-    struct adj_list *prox;
-};*/
 
 typedef struct verificados {
     int qtd;
     int lista[0];
 }VERF;
-
-//*****************************************************************************
-//                          FUN합ES P/ GRAPHVIZ
-//*****************************************************************************
-/*
-void add_color(int color_id, char *cor) {
-    struct col *s;
-    HASH_FIND_INT(colors, &color_id, s);
-    if (s == NULL) {
-        s = (struct col *) malloc(sizeof(struct col));
-        s->id = color_id;
-        strcpy(s->cor, cor);
-        HASH_ADD_INT(colors, id, s);
-    }
-}
-
-struct col *find_color(int color_id) {
-    struct col *s;
-
-    HASH_FIND_INT(colors, &color_id, s);
-    return s;
-}
-
-void fill_colors() {
-    int i;
-    char cor[25];
-    for (i = 1; i <= 10; i++) {
-        switch (i) {
-            case 1:
-                strcpy(cor, "black");
-                break;
-            case 2:
-                strcpy(cor, "blue");
-                break;
-            case 3:
-                strcpy(cor, "brown1");
-                break;
-            case 4:
-                strcpy(cor, "cadetblue1");
-                break;
-            case 5:
-                strcpy(cor, "chartreuse");
-                break;
-            case 6:
-                strcpy(cor, "crimson");
-                break;
-            case 7:
-                strcpy(cor, "darkolivegreen1");
-                break;
-            case 8:
-                strcpy(cor, "darkorchid");
-                break;
-            case 9:
-                strcpy(cor, "deeppink");
-                break;
-            case 10:
-                strcpy(cor, "gold");
-                break;
-            default:
-                break;
-        }
-        add_color(i, cor);
-    }
-}
-*/
-/**
-* Função para gerar o .png do grafo gerado quando o tipo de grafo for
-* CHEIO.
-**/
-/*
-void gera_graphviz_matriz(short int m[num_vertices][num_vertices], char * title) {
-    FILE *fp;
-    char *homepath = "";
-    short int i, u;
-    int id;
-    struct col *color;
-    char buf[200];
-
-    //fp = fopen("../gerados/grafo1.gv", "w");
-    if (is_windows == 1) {
-        homepath = getenv("homepath");
-        if (homepath == NULL) {
-            return;
-        }
-    }
-    char teste[10];
-    strcpy(teste, "teste");
-#ifdef linux
-    struct passwd *pw = getpwuid(getuid());
-    const char *homedir = pw->pw_dir;
-    strcpy(homepath, homedir);
-#endif // linux
-
-    char sep[2];
-    if (is_windows == 1) {
-        strcpy(sep, "\\");
-    } else {
-        strcpy(sep, "/");
-    }
-    int suffix = 0, fd;
-    sprintf(buf, "mkdir %s%sgrafosgerados", homepath, sep);
-    system(buf);
-    do {
-        suffix++;
-        sprintf(buf, "%s%sgrafosgerados%sgrafo%d.gv", homepath, sep, sep, suffix);
-        fd = open(buf, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
-    } while (fd < 0);
-    fp = fopen(buf, "w");
-    printf("graph G1 {\n");
-    fputs("graph G1 {\n", fp);
-    printf("\tGraph [label = \"%s\"];\n", title);
-    sprintf(buf, "\tGraph [label = \"%s\"];\n", title);
-    fputs(buf, fp);
-    for (i = 0; i < num_vertices; i++) {
-        id = (i + 1) % 10;
-        if ((i % 10) == 9) {
-            id = 10;
-        }
-        color = find_color(id);
-        printf("\tedge [color = ""%s"", fontcolor = ""%s""];\n",
-               color->cor, color->cor);
-        sprintf(buf, "\tedge [color = ""%s"", fontcolor = ""%s""];\n",
-                color->cor, color->cor);
-        fputs(buf, fp);
-
-        printf("\t%hd [color = ""%s"", fontcolor = ""%s""];\n",
-               i, color->cor, color->cor);
-        sprintf(buf, "\t%hd [color = ""%s"", fontcolor = ""%s""];\n",
-                i, color->cor, color->cor);
-        fputs(buf, fp);
-        for (u = i; u < num_vertices; u++) {
-            if (m[i][u] == 1) {
-                printf("\t%hd -- %hd;\n", i, u);
-                sprintf(buf, "\t%hd -- %hd;\n", i, u);
-                fputs(buf, fp);
-            }
-        }
-    }
-    printf("}\n");
-    fputs("}\n", fp);
-    fclose(fp);
-    sprintf(buf,
-            "dot -Tpng %s%sgrafosgerados%sgrafo%d.gv -o %s%sgrafosgerados%sgrafo%d.png",
-            homepath, sep, sep, suffix, homepath, sep, sep, suffix);
-    system(buf);
-    if (is_windows == 1) {
-        sprintf(buf, "%s%sgrafosgerados%sgrafo%d.png", homepath, sep, sep, suffix);
-    } else {
-        sprintf(buf, "eog %s%sgrafosgerados%sgrafo%d.png", homepath, sep, sep, suffix);
-    }
-    system(buf);
-}
-
-void graphviz_item_lista(FILE *fp, struct adj_list *e) {
-    struct adj_list *e1;
-    char buf[200];
-
-    if (e->prox == NULL) {
-        printf("\t%d;\n", e->cod);
-        sprintf(buf, "\t%d;\n", e->cod);
-        fputs(buf, fp);
-        return;
-    }
-
-    e1 = e->prox;
-
-    while (1) {
-        printf("\t%d -- %d;\n", e->cod, e1->cod);
-        sprintf(buf, "\t%d -- %d;\n", e->cod, e1->cod);
-        fputs(buf, fp);
-
-        if (e1->prox == NULL) {
-            break;
-        } else {
-            e1 = e1->prox;
-        }
-    }
-}
-*/
-/**
-* Função para gerar o .png do grafo gerado quando o tipo de grafo for
-* VAZIO.
-**/
-/*
-void gera_graphviz_lista(struct adj_list l[num_vertices], char * title) {
-    FILE *fp;
-    char *homepath = "";
-    int i, id;
-    struct col *color;
-    char buf[200];
-
-    if (is_windows == 1) {
-        homepath = getenv("homepath");
-        if (homepath == NULL) {
-            return;
-        }
-    }
-    char teste[10];
-    strcpy(teste, "teste");
-#ifdef linux
-    struct passwd *pw = getpwuid(getuid());
-    const char *homedir = pw->pw_dir;
-    strcpy(homepath, homedir);
-#endif // linux
-
-    char sep[2];
-    if (is_windows == 1) {
-        strcpy(sep, "\\");
-    } else {
-        strcpy(sep, "/");
-    }
-
-    sprintf(buf, "mkdir %s%sgrafosgerados", homepath, sep);
-    system(buf);
-    int suffix = 0, fd;
-    do {
-        suffix++;
-        sprintf(buf, "%s%sgrafosgerados%sgrafo%d.gv", homepath, sep, sep, suffix);
-        fd = open(buf, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
-    } while (fd < 0);
-    fp = fopen(buf, "w");
-    printf("strict graph G1 {\n");
-    fputs("strict graph G1 {\n", fp);
-    printf("\tGraph [label = \"%s\"];\n", title);
-    sprintf(buf, "\tGraph [label = \"%s\"];\n", title);
-    fputs(buf, fp);
-    for (i = 0; i < num_vertices; i++) {
-        id = (i + 1) % 10;
-        if ((i % 10) == 9) {
-            id = 10;
-        }
-        color = find_color(id);
-        printf("\tedge [color = ""%s"", fontcolor = ""%s""];\n",
-               color->cor, color->cor);
-        sprintf(buf, "\tedge [color = ""%s"", fontcolor = ""%s""];\n",
-                color->cor, color->cor);
-        fputs(buf, fp);
-
-        printf("\t%hd [color = ""%s"", fontcolor = ""%s""];\n",
-               i, color->cor, color->cor);
-        sprintf(buf, "\t%hd [color = ""%s"", fontcolor = ""%s""];\n",
-                i, color->cor, color->cor);
-        fputs(buf, fp);
-        graphviz_item_lista(fp, &l[i]);
-    }
-
-    printf("}\n");
-    fputs("}\n", fp);
-    fclose(fp);
-    sprintf(buf,
-            "dot -Tpng %s%sgrafosgerados%sgrafo%d.gv -o %s%sgrafosgerados%sgrafo%d.png",
-            homepath, sep, sep, suffix, homepath, sep, sep, suffix);
-    system(buf);
-    if (is_windows == 1) {
-        sprintf(buf, "%s%sgrafosgerados%sgrafo%d.png", homepath, sep, sep, suffix);
-    } else {
-        sprintf(buf, "eog %s%sgrafosgerados%sgrafo%d.png", homepath, sep, sep, suffix);
-    }
-    system(buf);
-}
-*/
-//*****************************************************************************
-//                          FUN합ES GERAIS
-//*****************************************************************************
 
 int ask_tipo() {
     short int is_correct = 0;
@@ -552,7 +274,6 @@ int main() {
         printf("ou a variável path não foi configurada\n.");
     }
     printf("Windows\n\n");
-    //is_windows = 1;
 #endif // _WIN32
 #ifdef linux
     printf("Linux\n");
@@ -597,7 +318,6 @@ int main() {
             strcpy(title, "GRAFO DESCONEXO");
         }
         if (answ == 's' || rc == 0) // Só gera o .png se o graphviz tiver instalado
-            //gera_graphviz_matriz(matriz, title);
             gera_graphviz_matriz(num_vertices, matriz, title);
     } else {
         struct adj_list adjacencias[num_vertices];
@@ -614,7 +334,6 @@ int main() {
             strcpy(title, "GRAFO DESCONEXO");
         }
         if (answ == 's' || rc == 0) // Só gera o .png se o graphviz tiver instalado
-            //gera_graphviz_lista(adjacencias, title);
             gera_graphviz_lista(num_vertices, adjacencias, title);
     }
 
